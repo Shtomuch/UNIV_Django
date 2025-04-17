8fa4d3a02801fabd64c35a6d0abb16b23f9893c7
IrusHunter <korinevskiy.roman@gmail.com>
2025-04-17 13:01:19 +0300

    added Order model

--- ci_cd_project2/models.py
+++ ci_cd_project2/models.py
@@ -54,3 +54,16 @@ class Notification(models.Model):
 
     def __str__(self):
         return self.message
+
+
+# Замовлення
+class Order(models.Model):
+    status = models.CharField(max_length=50)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return self.user.get_full_name() + " " + str(self.created_at)
